99c6b74d6eddfaa153f760e6f7e93553585893f7
Pranav Kapoor <pranavkapoorr@gmail.com>
2018-12-15 14:24:54 -0500

    startpopup ui

--- lib/views/mpos_home.dart
+++ lib/views/mpos_home.dart
@@ -29,7 +29,7 @@ class _MposHomeState extends State<MposHome>{
         appBar: new AppBar(
           elevation: 0.0,
         title: new Text("A-POS",style: new TextStyle(fontSize:30.0,color: Theme.of(context).accentColor),),),
-      body: (sIp != null && sPort != null && pIp !=null && pPort!=null)?_children[_currentIndex]:setUp(),
+      body: (sIp != null && sPort != null)?_children[_currentIndex]:setUp(),
       bottomNavigationBar: BottomNavigationBar(onTap: onTabTapped,
         currentIndex: _currentIndex,
         items: [
@@ -42,87 +42,60 @@ class _MposHomeState extends State<MposHome>{
   }
   Widget setUp()=>new Container(
       child: Padding(
-          padding: const EdgeInsets.all(8.0),
-          child: Container(
-              decoration: BoxDecoration(
-                  color: Colors.deepOrange.shade400,
-                  borderRadius: BorderRadius.circular(10.0)
-              ),
-              child: new ListView(
-                  children: <Widget>[
-                    Padding(
-                      padding: const EdgeInsets.all(8.0),
-                      child: Column(
-                        children: <Widget>[
-                          Padding(
-                            padding: const EdgeInsets.all(8.0),
-                            child: Align(alignment: Alignment.topCenter,child: Text("QUICK SETUP!",style: TextStyle(color:Colors.white,fontSize: 25.0,fontWeight: FontWeight.w500),)),
-                          ),
-                          Align(alignment: Alignment.topLeft,child: Text("Server EndPoints",style: TextStyle(color:Colors.white,fontSize: 15.0,fontWeight: FontWeight.w500),)),
-                          Padding(
-                            padding: const EdgeInsets.all(8.0),
-                            child: Container(
-                                padding: EdgeInsets.all(8.0),
-                                decoration: BoxDecoration(
-                                    color: Colors.white,
-                                    border: Border.all(color: Colors.grey.shade200),
-                                    borderRadius: BorderRadius.only(topLeft: Radius.circular(8.0))
-                                ),
-                                child: TextField(decoration: InputDecoration.collapsed(hintText: "Enter Ip address"),controller: null,)
+          padding: const EdgeInsets.all(15.0),
+          child: Center(
+            child: Container(
+                decoration: BoxDecoration(
+                    color: Colors.deepOrange.shade400,
+                    borderRadius: BorderRadius.circular(10.0)
+                ),
+                child: new Column(
+                    mainAxisSize: MainAxisSize.min,
+                    children: <Widget>[
+                      Padding(
+                        padding: const EdgeInsets.all(8.0),
+                        child: Column(
+                          children: <Widget>[
+                            Padding(
+                              padding: const EdgeInsets.all(8.0),
+                              child: Align(alignment: Alignment.topCenter,child: Text("QUICK SETUP!",style: TextStyle(color:Colors.white,fontSize: 25.0,fontWeight: FontWeight.w500),)),
                             ),
-                          ),
-                          Padding(
-                            padding: const EdgeInsets.all(8.0),
-                            child: Container(
-                                padding: EdgeInsets.all(8.0),
-                                decoration: BoxDecoration(
-                                    color: Colors.white,
-                                    border: Border.all(color: Colors.grey.shade200),
-                                    borderRadius: BorderRadius.only(bottomRight: Radius.circular(8.0))
-                                ),
-                                child: TextField(decoration: InputDecoration.collapsed(hintText: "Enter Port"),controller: null,)
+                            Align(alignment: Alignment.topLeft,child: Text("Server EndPoints",style: TextStyle(color:Colors.white,fontSize: 15.0,fontWeight: FontWeight.w500),)),
+                            Padding(
+                              padding: const EdgeInsets.all(8.0),
+                              child: Container(
+                                  padding: EdgeInsets.all(8.0),
+                                  decoration: BoxDecoration(
+                                      color: Colors.white,
+                                      border: Border.all(color: Colors.grey.shade200),
+                                      borderRadius: BorderRadius.only(topLeft: Radius.circular(8.0))
+                                  ),
+                                  child: TextField(decoration: InputDecoration.collapsed(hintText: "Enter Ip address"),controller: null,)
+                              ),
                             ),
-                          ),
-                          Text("What is the service ip adress and port..?",style: TextStyle(color: Colors.white70,fontSize: 12.0),)
-                        ],
+                            Padding(
+                              padding: const EdgeInsets.all(8.0),
+                              child: Container(
+                                  padding: EdgeInsets.all(8.0),
+                                  decoration: BoxDecoration(
+                                      color: Colors.white,
+                                      border: Border.all(color: Colors.grey.shade200),
+                                      borderRadius: BorderRadius.only(bottomRight: Radius.circular(8.0))
+                                  ),
+                                  child: TextField(decoration: InputDecoration.collapsed(hintText: "Enter Port"),controller: null,)
+                              ),
+                            ),
+                            Text("What is the service ip adress and port..?",style: TextStyle(color: Colors.white70,fontSize: 12.0),)
+                          ],
+                        ),
                       ),
-                    ),
-                    Padding(
-                      padding: const EdgeInsets.all(8.0),
-                      child: Column(
-                        children: <Widget>[
-                          Align(alignment: Alignment.topLeft,child: Text("Device EndPoints",style: TextStyle(color:Colors.white,fontSize: 15.0,fontWeight: FontWeight.w500),)),
-                          Padding(
-                            padding: const EdgeInsets.all(8.0),
-                            child: Container(
-                                padding: EdgeInsets.all(8.0),
-                                decoration: BoxDecoration(
-                                    color: Colors.white,
-                                    border: Border.all(color: Colors.grey.shade200),
-                                    borderRadius: BorderRadius.only(topLeft: Radius.circular(8.0))
-                                ),
-                                child: TextField(decoration: InputDecoration.collapsed(hintText: "Enter Ip address"),controller:null,)
-                            ),
-                          ),
-                          Padding(
-                            padding: const EdgeInsets.all(8.0),
-                            child: Container(
-                                padding: EdgeInsets.all(8.0),
-                                decoration: BoxDecoration(
-                                    color: Colors.white,
-                                    border: Border.all(color: Colors.grey.shade200),
-                                    borderRadius: BorderRadius.only(bottomRight: Radius.circular(8.0))
-                                ),
-                                child: TextField(decoration: InputDecoration.collapsed(hintText: "Enter Port"),controller: null,)
-                            ),
-                          ),
-                          Text("What is the payment device ip adress and port..?",style: TextStyle(color: Colors.white70,fontSize: 12.0),)
-                        ],
-                      ),
-                    ),
-                    Align(alignment: Alignment.center,child: FlatButton(textColor: Colors.white,onPressed: (){}, child: Text('Submit')),)
-                  ]
-              ))
+                      Padding(
+                        padding: const EdgeInsets.all(8.0),
+                        child: Align(alignment: Alignment.center,child: FlatButton(shape: Border.all(color: Colors.white),splashColor: Colors.white,textColor: Colors.white,onPressed: (){}, child: Text('Submit')),),
+                      )
+                    ]
+                )),
+          )
       )
   );
 
